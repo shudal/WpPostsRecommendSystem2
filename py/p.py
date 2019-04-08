8a6f088deee6f616f6366be2e335990c1bcfdace
shudal <shudal@qq.com>
2019-04-08 16:58:15 +0800

    【feat】获取用户like和unlike的文章，对每篇文章打分后排序并写进数据库

--- py/p.py
+++ py/p.py
@@ -26,10 +26,13 @@ db = pymysql.connect(host = db_config['host'],port = db_config['port'], user = d
 cursor = db.cursor()
 
 
+print("Query posts...")
 import datetime
 today = datetime.date.today()
 preweek = today - datetime.timedelta(days=8)
-sql_query_posts = "select ID,post_content from " + db_config['prefix'] + "_posts p where p.post_date > '" + str(preweek)  + "' and post_status='publish' and post_type='post'"
+#sql_query_posts = "select ID,post_content from " + db_config['prefix'] + "_posts p where p.post_date > '" + str(preweek)  + "' and post_status='publish' and post_type='post'"
+
+sql_query_posts = "select ID,post_content from " + db_config['prefix'] + "_posts where post_status='publish' and post_type='post'"
 cursor.execute(sql_query_posts)
 posts = cursor.fetchall()
 
@@ -44,16 +47,77 @@ for i in range(0, len(posts)):
 print("The number of posts:" + str(len(post_contents)))
 
 tfidf_matrix = tf.fit_transform(post_contents)
-print(id2index)
 
 from sklearn.metrics.pairwise import linear_kernel
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-def get_recommendations(postid, cosine_sim = cosine_sim):
+def get_recos(postid, cosine_sim = cosine_sim):
 	idx = id2index[str(postid)]
 	sim_scores = list(enumerate(cosine_sim[idx]))
 	sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-	sim_scores = sim_scores[1:11]	
-	post_indices = [i[0] for i in sim_scores]
-	return post_indices
+	return sim_scores
 
+sql_query_users = "select user_id from " + db_config['prefix'] + "_ulike";
+cursor.execute(sql_query_users)
+users3 = cursor.fetchall()
+users2 = []
+for i in range(0, len(users3)):
+	userid = int(users3[i][0])
+	if userid>100000000:
+		continue
+	users2.append(userid);
+
+userids = list(set(users2))
+userids.sort(key=users2.index)
+
+print("The number of users :" + str(len(userids)));
+
+for i in range(0, len(userids)):
+	
+	sql_query_likes = "select post_id from " + db_config['prefix'] + "_ulike where user_id='" + str(userids[i])  +"' and status='like'"
+	cursor.execute(sql_query_likes)
+	likePostsIds2 = cursor.fetchall()
+	likePosts  = {}
+	for k in range(0, len(likePostsIds2)):
+		a = get_recos(likePostsIds2[k][0])
+
+		for l in range(0, len(a)):
+			likePosts[a[l][0]] = likePosts.get(a[l][0], 0) + a[l][1]
+
+	#print(likePosts)			
+	sql_query_likes = "select post_id from " + db_config['prefix'] + "_ulike where user_id='" + str(userids[i])  +"' and status='unlike'"
+	cursor.execute(sql_query_likes)
+	unlikePostsIds2 = cursor.fetchall()
+	for k in range(0, len(unlikePostsIds2)):
+		a = get_recos(unlikePostsIds2[k][0])
+
+		for l in range(0, len(a)):
+			likePosts[a[l][0]] = likePosts.get(a[l][0], 0) - a[l][1]
+
+	#print(likePosts)
+	likePosts = sorted(likePosts.items(), key=lambda item: item[1], reverse=True)
+	
+	recoPostsIds = []
+	for k in range(0, len(likePosts)):
+		recoPostsIds.append(likePosts[k][0])
+	#print(likePosts)
+	str_reco_posts = ""
+	for k in range(0, len(recoPostsIds)):
+		str_reco_posts += ("|" + str(recoPostsIds[k]))
+
+	sql_get_cbreco = "select meta_value from " + db_config['prefix'] + "_usermeta where user_id='" + str(userids[i]) + "' and meta_key='_perci_haku_cbreco'"
+	cursor.execute(sql_get_cbreco)
+	data = cursor.fetchone()
+
+	if data is None:
+		sql_set_cbreco = "insert into " + db_config['prefix'] + "_usermeta(`user_id`,`meta_key`,`meta_value`) values('" + str(userids[i])  + "', '_perci_haku_cbreco','" + str_reco_posts + "')"
+	else :
+		sql_set_cbreco = "update " + db_config['prefix'] + "_usermeta set meta_value='" + str_reco_posts + "' where user_id='" + str(userids[i])  +  "' and meta_key='_perci_haku_cbreco'"
+
+	print(str_reco_posts)
+	try :
+		cursor.execute(sql_set_cbreco)
+		db.commit()
+	except Exception as e:
+		print(str(e))
+		db.rollback()
